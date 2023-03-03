fb9ef73d5b176823ee7583d3e8a5a3f564ea19dd
Brendan Baird <brendan.b517@gmail.com>
2023-03-02 19:07:12 -0500

    commit tree a41833cdf59f7d31e359edb69f42e596ddeaabc7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def grades
+  end
+
+  def highest_grade
+  end
+
 end
